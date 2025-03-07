341eaa5ac599072b0a07ec9fcffcc9c7d1c54f51
unknown <carlogquilla@gmail.com>
2025-03-07 15:23:18 +0800

    first commit

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Edit A Product</h1>
+    <form method="POST" action="{{ route('products.update', ['product' => $product]) }}">
+        @csrf
+        @method('PUT')
+        <div>
+            @if($errors->any())
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name" value="{{ $product->name }}" />
+        </div>
+        <div>
+            <label>Qty</label>
+            <input type="text" name="qty" placeholder="Qty" value="{{ $product->qty }}" />
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="text" name="price" placeholder="Price" value="{{ $product->price }}" />
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="text" name="description" placeholder="Description" value="{{ $product->description }}" />
+        </div>
+        <div>
+            <input type="submit" value="Edit the Product" />
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Product</h1>
+    <div>
+        @if (session()->has('success'))
+            {{ session('success') }}
+        @endif
+    </div>
+    <div>
+        <a href='{{ route('products.create') }}'>Create A Product</a>
+    </div>
+    <div>Index</div>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+        @foreach ($products as $product)
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->name}}</td>
+                <td>{{$product->qty}}</td>
+                <td>{{$product->price}}</td>
+                <td>{{$product->description}}</td>
+                <td>
+                    <a href="{{route('products.edit', ['product' => $product])}}">Edit</a>
+                </td>
+                <td>
+                    <form method='POST' action='{{ route('products.delete', ['product' => $product]) }}'>
+                        @csrf
+                        @method('delete')
+                        <input type='submit' value='Delete' />
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+
+</html>
